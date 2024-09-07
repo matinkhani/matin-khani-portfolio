1332a778930ad763acbb95a7dc6fe5c47391489a
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-07 12:28:25 +0330

    enable prettier-plugin-tailwindcss for input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,14 +11,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-sm border-2 border-Primary-Black bg-transparent hover:bg-Zinc-100 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-Zinc-500 text-Primary-Black focus:shadow-md disabled:cursor-not-allowed disabled:border-Zinc-200 disabled:bg-Zinc-100 disabled:text-Zinc-300",
-          className
+          "flex h-9 w-full rounded-sm border-2 border-Primary-Black bg-transparent px-3 py-1 text-sm text-Primary-Black shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-Zinc-500 hover:bg-Zinc-100 focus:shadow-md disabled:cursor-not-allowed disabled:border-Zinc-200 disabled:bg-Zinc-100 disabled:text-Zinc-300",
+          className,
         )}
         ref={ref}
         {...props}
       />
     );
-  }
+  },
 );
 Input.displayName = "Input";
 
