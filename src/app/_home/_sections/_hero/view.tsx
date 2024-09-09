7e02172f545431b258015e1f08cefec2b1b2b64f
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-09 13:43:04 +0330

    remove background colors from HeroView section

--- src/app/_home/_sections/_hero/view.tsx
+++ src/app/_home/_sections/_hero/view.tsx
@@ -3,23 +3,24 @@ import React from "react";
 
 const HeroView = () => {
   return (
-    <div className="flex w-full flex-col items-center bg-gray-300 md:flex-row-reverse">
+    // TODO: add text color to texts
+    <div className="flex w-full flex-col items-center md:flex-row-reverse">
       <Image
         src="/images/hero-section.png"
         alt="hero image"
         width={343}
         height={332}
-        className="block bg-green-300 md:hidden"
+        className="block md:hidden"
       />
       <Image
         src="/images/hero-section-desktop.png"
         alt="hero image"
         width={889}
         height={596}
-        className="hidden bg-green-300 md:block"
+        className="hidden md:block"
       />
-      <div className="mt-12 flex flex-col gap-8 bg-sky-300 md:mt-0">
-        <div className="bg-yellow-200">
+      <div className="mt-12 flex flex-col gap-8 md:mt-0">
+        <div>
           <span className="text-DisplayText(M)-Regular">Hello I am</span>
           <span className="text-DisplayText(M)-ExtraBold ml-2">
             Matin Khani.
@@ -33,7 +34,7 @@ const HeroView = () => {
             Iran, Tehran.
           </span>
         </div>
-        <p className="text-Paragraph-P2-Regular bg-orange-300 text-Zinc-500">
+        <p className="text-Paragraph-P2-Regular text-Zinc-500">
           I'm 20 years old, a web developer specializing in front-end
           development. I became interested in programming and choose it as my
           life path. Always energetic and eager to learn new skills and solve
@@ -41,7 +42,7 @@ const HeroView = () => {
           from the ability to continously learn, adapt, and master new
           technologies, not just from knowing everything.
         </p>
-        <div className="h-14 w-full bg-indigo-400"></div>
+        <div className="h-14 w-full bg-green-300"></div>
       </div>
     </div>
   );
