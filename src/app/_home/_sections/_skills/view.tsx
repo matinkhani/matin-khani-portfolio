a7163ede3d042f1e51540d13a699e1e9e4f7ad1c
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-09 13:38:48 +0330

    create basic of skills view

---
+++ src/app/_home/_sections/_skills/view.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SkillsView = () => {
+  return (
+    <div className="mt-10 bg-red-400">
+      <div className="flex h-72 w-full items-center justify-center bg-sky-300">
+        <span className="text-DisplayText(M)-Regular text-Primary-Black">
+          My
+        </span>
+        <span className="text-DisplayText(M)-ExtraBold ml-2 text-Primary-Black">
+          Skills
+        </span>
+      </div>
+      <div className="h-64 w-full bg-yellow-200"></div>
+    </div>
+  );
+};
+
+export default SkillsView;
