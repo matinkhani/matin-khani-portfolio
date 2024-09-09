308ec9e192a3ba2b79ccad929af82c33ab375c77
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-09 10:51:53 +0330

    add figma typography in tailwind confgi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      spacing: {
+        186: "186px",
+        1440: "1440px",
+      },
       colors: {
         // Figma Colors:
         Primary: {
@@ -25,6 +29,14 @@ const config: Config = {
         },
         Error: {
           500: "#ef4444",
+        },
+        // outline font:
+        textStrokeWidth: {
+          "1": "1px",
+          "2": "2px",
+        },
+        colors: {
+          strokeColor: "#000000", // Customize this to match your design
         },
         // Shadcn Colors:
         background: "hsl(var(--background))",
@@ -68,6 +80,414 @@ const config: Config = {
           "5": "hsl(var(--chart-5))",
         },
       },
+      fontSize: {
+        // Display Text:
+        "DisplayText-Regular": [
+          "3rem",
+          {
+            fontWeight: 400,
+            lineHeight: "3.5rem",
+          },
+        ],
+        "DisplayText-SemiBold": [
+          "3rem",
+          {
+            fontWeight: 600,
+            lineHeight: "3.5rem",
+          },
+        ],
+        "DisplayText-Bold": [
+          "3rem",
+          {
+            fontWeight: 700,
+            lineHeight: "3.5rem",
+          },
+        ],
+        "DisplayText-ExtraBold": [
+          "3rem",
+          {
+            fontWeight: 800,
+            lineHeight: "3.5rem",
+          },
+        ],
+        // Display Text Medium:
+        "DisplayText(M)-Regular": [
+          "1.5rem",
+          {
+            fontWeight: 400,
+            lineHeight: "1.75rem",
+          },
+        ],
+        "DisplayText(M)-SemiBold": [
+          "1.5rem",
+          {
+            fontWeight: 600,
+            lineHeight: "1.75rem",
+          },
+        ],
+        "DisplayText(M)-Bold": [
+          "1.5rem",
+          {
+            fontWeight: 700,
+            lineHeight: "1.75rem",
+          },
+        ],
+        "DisplayText(M)-ExtraBold": [
+          "1.5rem",
+          {
+            fontWeight: 800,
+            lineHeight: "1.75rem",
+          },
+        ],
+        // Sub Heading:
+        "SubHeading-Regular": [
+          "2.5rem",
+          {
+            fontWeight: 400,
+            lineHeight: "3rem",
+          },
+        ],
+        "SubHeading-SemiBold": [
+          "2.5rem",
+          {
+            fontWeight: 600,
+            lineHeight: "3rem",
+          },
+        ],
+        "SubHeading-Bold": [
+          "2.5rem",
+          {
+            fontWeight: 700,
+            lineHeight: "3rem",
+          },
+        ],
+        "SubHeading-ExtraBold": [
+          "2.5rem",
+          {
+            fontWeight: 800,
+            lineHeight: "3rem",
+          },
+        ],
+        // Heading H1:
+        "Heading-H1-Regular": [
+          "2.25rem",
+          {
+            fontWeight: 400,
+            lineHeight: "2.75rem",
+          },
+        ],
+        "Heading-H1-SemiBold": [
+          "2.25rem",
+          {
+            fontWeight: 600,
+            lineHeight: "2.75rem",
+          },
+        ],
+        "Heading-H1-Bold": [
+          "2.25rem",
+          {
+            fontWeight: 700,
+            lineHeight: "2.75rem",
+          },
+        ],
+        "Heading-H1-ExtraBold": [
+          "2.25rem",
+          {
+            fontWeight: 800,
+            lineHeight: "2.75rem",
+          },
+        ],
+        // Heading H2:
+        "Heading-H2-Regular": [
+          "2rem",
+          {
+            fontWeight: 400,
+            lineHeight: "2.5rem",
+          },
+        ],
+        "Heading-H2-SemiBold": [
+          "2rem",
+          {
+            fontWeight: 600,
+            lineHeight: "2.5rem",
+          },
+        ],
+        "Heading-H2-Bold": [
+          "2rem",
+          {
+            fontWeight: 700,
+            lineHeight: "2.5rem",
+          },
+        ],
+        "Heading-H2-ExtraBold": [
+          "2rem",
+          {
+            fontWeight: 800,
+            lineHeight: "2.5rem",
+          },
+        ],
+        // Heading H3:
+        "Heading-H3-Regular": [
+          "1.75rem",
+          {
+            fontWeight: 400,
+            lineHeight: "2rem",
+          },
+        ],
+        "Heading-H3-SemiBold": [
+          "1.75rem",
+          {
+            fontWeight: 600,
+            lineHeight: "2rem",
+          },
+        ],
+        "Heading-H3-Bold": [
+          "1.75rem",
+          {
+            fontWeight: 700,
+            lineHeight: "2rem",
+          },
+        ],
+        "Heading-H3-ExtraBold": [
+          "1.75rem",
+          {
+            fontWeight: 800,
+            lineHeight: "2rem",
+          },
+        ],
+        // Heading H4:
+        "Heading-H4-Regular": [
+          "1.5rem",
+          {
+            fontWeight: 400,
+            lineHeight: "1.75rem",
+          },
+        ],
+        "Heading-H4-SemiBold": [
+          "1.5rem",
+          {
+            fontWeight: 600,
+            lineHeight: "1.75rem",
+          },
+        ],
+        "Heading-H4-Bold": [
+          "1.5rem",
+          {
+            fontWeight: 700,
+            lineHeight: "1.75rem",
+          },
+        ],
+        "Heading-H4-ExtraBold": [
+          "1.5rem",
+          {
+            fontWeight: 800,
+            lineHeight: "1.75rem",
+          },
+        ],
+        // Heading H5:
+        "Heading-H5-Regular": [
+          "1.25rem",
+          {
+            fontWeight: 400,
+            lineHeight: "1.5rem",
+          },
+        ],
+        "Heading-H5-SemiBold": [
+          "1.25rem",
+          {
+            fontWeight: 600,
+            lineHeight: "1.5rem",
+          },
+        ],
+        "Heading-H5-Bold": [
+          "1.25rem",
+          {
+            fontWeight: 700,
+            lineHeight: "1.5rem",
+          },
+        ],
+        "Heading-H5-ExtraBold": [
+          "1.25rem",
+          {
+            fontWeight: 800,
+            lineHeight: "1.5rem",
+          },
+        ],
+        // Heading H6:
+        "Heading-H6-Regular": [
+          "1rem",
+          {
+            fontWeight: 400,
+            lineHeight: "1.25rem",
+          },
+        ],
+        "Heading-H6-SemiBold": [
+          "1rem",
+          {
+            fontWeight: 600,
+            lineHeight: "1.25rem",
+          },
+        ],
+        "Heading-H6-Bold": [
+          "1rem",
+          {
+            fontWeight: 700,
+            lineHeight: "1.25rem",
+          },
+        ],
+        "Heading-H6-ExtraBold": [
+          "1rem",
+          {
+            fontWeight: 800,
+            lineHeight: "1.25rem",
+          },
+        ],
+        // Paragraph P1:
+        "Paragraph-P1-Regular": [
+          "1.25rem",
+          {
+            fontWeight: 400,
+            lineHeight: "1.5rem",
+          },
+        ],
+        "Paragraph-P1-SemiBold": [
+          "1.25rem",
+          {
+            fontWeight: 600,
+            lineHeight: "1.5rem",
+          },
+        ],
+        "Paragraph-P1-Bold": [
+          "1.25rem",
+          {
+            fontWeight: 700,
+            lineHeight: "1.5rem",
+          },
+        ],
+        "Paragraph-P1-ExtraBold": [
+          "1.25rem",
+          {
+            fontWeight: 800,
+            lineHeight: "1.5rem",
+          },
+        ],
+        // Paragraph P2:
+        "Paragraph-P2-Regular": [
+          "1rem",
+          {
+            fontWeight: 400,
+            lineHeight: "1.25rem",
+          },
+        ],
+        "Paragraph-P2-SemiBold": [
+          "1rem",
+          {
+            fontWeight: 600,
+            lineHeight: "1.25rem",
+          },
+        ],
+        "Paragraph-P2-Bold": [
+          "1rem",
+          {
+            fontWeight: 700,
+            lineHeight: "1.25rem",
+          },
+        ],
+        "Paragraph-P2-ExtraBold": [
+          "1rem",
+          {
+            fontWeight: 800,
+            lineHeight: "1.25rem",
+          },
+        ],
+        // Paragraph P3:
+        "Paragraph-P3-Regular": [
+          "0.75rem",
+          {
+            fontWeight: 400,
+            lineHeight: "1rem",
+          },
+        ],
+        "Paragraph-P3-SemiBold": [
+          "0.75rem",
+          {
+            fontWeight: 600,
+            lineHeight: "1rem",
+          },
+        ],
+        "Paragraph-P3-Bold": [
+          "0.75rem",
+          {
+            fontWeight: 700,
+            lineHeight: "1rem",
+          },
+        ],
+        "Paragraph-P3-ExtraBold": [
+          "0.75rem",
+          {
+            fontWeight: 800,
+            lineHeight: "1rem",
+          },
+        ],
+        // Button Text:
+        "Button-Text-Regular": [
+          "1.25rem",
+          {
+            fontWeight: 400,
+            lineHeight: "1.5rem",
+          },
+        ],
+        "Button-Text-SemiBold": [
+          "1.25rem",
+          {
+            fontWeight: 600,
+            lineHeight: "1.5rem",
+          },
+        ],
+        "Button-Text-Bold": [
+          "1.25rem",
+          {
+            fontWeight: 700,
+            lineHeight: "1.5rem",
+          },
+        ],
+        "Button-Text-ExtraBold": [
+          "1.25rem",
+          {
+            fontWeight: 800,
+            lineHeight: "1.5rem",
+          },
+        ],
+        // Button Text 2:
+        "Button-Text-2-Regular": [
+          "1rem",
+          {
+            fontWeight: 400,
+            lineHeight: "1.25rem",
+          },
+        ],
+        "Button-Text-2-SemiBold": [
+          "1rem",
+          {
+            fontWeight: 600,
+            lineHeight: "1.25rem",
+          },
+        ],
+        "Button-Text-2-Bold": [
+          "1rem",
+          {
+            fontWeight: 700,
+            lineHeight: "1.25rem",
+          },
+        ],
+        "Button-Text-2-ExtraBold": [
+          "1rem",
+          {
+            fontWeight: 800,
+            lineHeight: "1.25rem",
+          },
+        ],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -75,6 +495,24 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ addUtilities }: { addUtilities: (utilities: object) => void }) {
+      const newUtilities = {
+        ".text-outline": {
+          "-webkit-text-stroke": "1px black",
+          "font-weight": "700",
+          color: "white",
+          "@variants responsive, hover": {},
+        },
+        ".text-outline-thick": {
+          "-webkit-text-stroke": "2px black",
+          color: "transparent",
+          "@variants responsive, hover": {},
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
 };
 export default config;
