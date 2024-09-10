46cc006d35c05c9a5a33c3ec95bfff9a30b2d809
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-10 11:47:54 +0330

    edited hero section

--- src/app/_home/_sections/_hero/view.tsx
+++ src/app/_home/_sections/_hero/view.tsx
@@ -1,40 +1,61 @@
+import IconGithub from "@/components/icons/icon-github";
+import IconLinkedin from "@/components/icons/icon-linkedin";
+import IconMedium from "@/components/icons/icon-medium";
+import IconTelegram from "@/components/icons/icon-telegram";
+import CardSocials from "@/components/ui/card-socials";
 import Image from "next/image";
 import React from "react";
+
+// TODO: edit items array
+const SocialItems = [
+  { id: 1, icon: <IconGithub />, link: "https://github.com/matinkhani" },
+  { id: 2, icon: <IconLinkedin />, link: "https://linkedin.com/in/matinkhani" },
+  { id: 3, icon: <IconMedium />, link: "" },
+  { id: 4, icon: <IconTelegram />, link: "" },
+];
 
 const HeroView = () => {
   return (
     // TODO: add text color to texts
     <div className="flex w-full flex-col items-center md:flex-row-reverse">
       <Image
-        src="/images/hero-section.png"
+        src="/images/hero-section-2.jpg"
         alt="hero image"
-        width={343}
-        height={332}
+        width={350}
+        height={280}
         className="block md:hidden"
       />
       <Image
-        src="/images/hero-section-desktop.png"
+        src="/images/hero-section-2.jpg"
         alt="hero image"
-        width={889}
-        height={596}
+        width={500}
+        height={496}
         className="hidden md:block"
       />
       <div className="mt-12 flex flex-col gap-8 md:mt-0">
         <div>
-          <span className="text-DisplayText(M)-Regular">Hello I am</span>
-          <span className="text-DisplayText(M)-ExtraBold ml-2">
+          <span className="text-DisplayText(M)-Regular md:text-DisplayText-Regular">
+            Hello I am
+          </span>
+          <span className="ml-2 text-DisplayText(M)-ExtraBold md:text-DisplayText-ExtraBold">
             Matin Khani.
           </span>
           <br />
-          <span className="text-DisplayText(M)-ExtraBold">Frontend</span>
-          <span className="text-DisplayText(M)-Regular ml-2">Developer</span>
+          <span className="text-DisplayText(M)-ExtraBold md:text-DisplayText-ExtraBold">
+            Frontend
+          </span>
+          <span className="ml-2 text-DisplayText(M)-Regular md:text-DisplayText-Regular">
+            Developer
+          </span>
           <br />
-          <span className="text-DisplayText(M)-Regular">Based In</span>
-          <span className="text-DisplayText(M)-ExtraBold ml-2">
+          <span className="text-DisplayText(M)-Regular md:text-DisplayText-Regular">
+            Based In
+          </span>
+          <span className="ml-2 text-DisplayText(M)-ExtraBold md:text-DisplayText-ExtraBold">
             Iran, Tehran.
           </span>
         </div>
-        <p className="text-Paragraph-P2-Regular text-Zinc-500">
+        <p className="text-Paragraph-P2-Regular text-Zinc-500 md:text-Paragraph-P1-Regular">
           I am 20 years old, a web developer specializing in front-end
           development. I became interested in programming and choose it as my
           life path. Always energetic and eager to learn new skills and solve
@@ -42,7 +63,16 @@ const HeroView = () => {
           from the ability to continously learn, adapt, and master new
           technologies, not just from knowing everything.
         </p>
-        <div className="h-14 w-full bg-green-300"></div>
+        <div className="flex h-14 w-full items-center gap-6 md:gap-8">
+          {SocialItems.map((item) => (
+            <CardSocials
+              icon={item.icon}
+              link={item.link}
+              iconClassName="group-hover:fill-Primary-White"
+              key={item.id}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
