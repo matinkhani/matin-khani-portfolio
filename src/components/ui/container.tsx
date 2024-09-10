45bee30b19fca42615c0a113219e78de252ce965
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-10 11:11:19 +0330

    edit container

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -2,9 +2,7 @@ import React, { FC, PropsWithChildren } from "react";
 
 const Container: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="m-auto min-h-screen w-full max-w-1440">
-      <div className="px-4 py-10 md:px-20 md:py-[60px]">{children}</div>
-    </div>
+    <div className="m-auto min-h-screen w-full max-w-1440">{children}</div>
   );
 };
 
