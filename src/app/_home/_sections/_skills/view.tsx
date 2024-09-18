b23c66248cb01d2af382c4717c2e609b14258f6c
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-18 10:03:33 +0330

    edit hero view

--- src/app/_home/_sections/_skills/view.tsx
+++ src/app/_home/_sections/_skills/view.tsx
@@ -1,17 +1,37 @@
 import React from "react";
+import { SkillsT } from "@/types/model/skills.type";
+import CardSkills from "@/components/ui/card-skills";
 
-const SkillsView = () => {
+async function getSkills() {
+  try {
+    let res = await fetch("http://localhost:3000/api/skills");
+    let data = await res.json();
+    return data;
+  } catch (error) {
+    throw new Error("Failed to fetch skills");
+  }
+}
+
+const SkillsView = async () => {
+  const skills = await getSkills();
+
   return (
-    <div className="mt-10 bg-red-400">
-      <div className="flex h-72 w-full items-center justify-center bg-sky-300">
+    <div className="mt-10">
+      <div className="flex h-72 w-full items-center justify-center">
         <span className="text-DisplayText(M)-Regular text-Primary-Black">
           My
         </span>
-        <span className="text-DisplayText(M)-ExtraBold ml-2 text-Primary-Black">
+        <span className="ml-2 text-DisplayText(M)-ExtraBold text-Primary-Black">
           Skills
         </span>
       </div>
-      <div className="h-64 w-full bg-yellow-200"></div>
+
+      <div className="mt-4 flex w-full flex-wrap justify-center gap-5">
+        {skills.res &&
+          skills.res.map((skill: SkillsT) => (
+            <CardSkills title={skill.title} key={skill.id} />
+          ))}
+      </div>
     </div>
   );
 };
