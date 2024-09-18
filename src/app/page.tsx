607c9fe0be8de5c1ead3fcac7b30dc286220e77f
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-18 11:52:50 +0330

    add experience view in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Suspense } from "react";
 import HeroView from "./_home/_sections/_hero/view";
 import SkillsView from "./_home/_sections/_skills/view";
+import ExperiencesView from "./_home/_sections/_experience/view";
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
+    <main className="flex flex-col items-center">
       <HeroView />
       <Suspense fallback={"Loading..."}>
         <SkillsView />
       </Suspense>
+      <ExperiencesView />
     </main>
   );
 }
