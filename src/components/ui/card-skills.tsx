2baaa80fd07a1550989bd71ec621bec170358a87
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-23 11:01:03 +0330

    change w-28 to flex-grow

--- src/components/ui/card-skills.tsx
+++ src/components/ui/card-skills.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const CardSkills: FC<Props> = ({ title, icon }) => {
   return (
-    <div className="group flex h-28 w-28 cursor-custom flex-col items-center justify-center gap-9 rounded border-2 border-Primary-Black bg-Primary-White p-6 hover:bg-Primary-Black md:h-36 md:w-36">
+    <div className="cursor-custom group flex h-28 flex-grow flex-col items-center justify-center rounded border-2 border-Primary-Black bg-Primary-White p-6 hover:bg-Primary-Black md:h-36">
       {icon}
       <p className="text-Primary-Black group-hover:text-Primary-White">
         {title}
