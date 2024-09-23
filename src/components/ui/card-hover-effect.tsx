51a5651779cf55b2c578391d02ee0d931e9194c7
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-23 14:42:24 +0330

    add Aceternity UI hover effect card

---
+++ src/components/ui/card-hover-effect.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { SkillsT } from "@/types/model/skills.type";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
+export const HoverEffect = ({
+  items,
+  className,
+}: {
+  items: SkillsT[];
+  className?: string;
+}) => {
+  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-6",
+        className,
+      )}
+    >
+      {items.map((item, idx) => (
+        <div
+          key={item?.id}
+          className="group relative block h-full w-full p-2"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 block h-full w-full rounded-lg bg-Primary-Neutral dark:bg-slate-800/[0.8]"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <Card>
+            <CardTitle>{item.title}</CardTitle>
+            {/* <CardDescription>{item.description}</CardDescription> */}
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const Card = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "relative z-20 flex h-36 w-full items-center justify-center overflow-hidden rounded border-2 border-Primary-Black bg-Primary-White p-4 md:h-36",
+        className,
+      )}
+    >
+      <div className="relative z-50">
+        <div className="p-4">{children}</div>
+      </div>
+    </div>
+  );
+};
+export const CardTitle = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <h4
+      className={cn(
+        "mt-4 font-bold tracking-wide text-Primary-Black",
+        className,
+      )}
+    >
+      {children}
+    </h4>
+  );
+};
