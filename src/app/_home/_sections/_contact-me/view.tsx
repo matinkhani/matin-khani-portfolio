0e741380d61095408e9d0107bd1f7e9f38c4f1f5
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-23 15:16:50 +0330

    change p to link in contact me for email

--- src/app/_home/_sections/_contact-me/view.tsx
+++ src/app/_home/_sections/_contact-me/view.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormContactMe from "./form-contact-me";
+import Link from "next/link";
 
 const ContactMeView = () => {
   return (
@@ -8,7 +9,7 @@ const ContactMeView = () => {
       className="mt-10 flex w-full flex-col items-center bg-Primary-White md:flex-row md:gap-28"
     >
       <FormContactMe />
-      <div className="flex flex-col gap-8">
+      <div className="mt-3 flex flex-col gap-8">
         <p className="text-DisplayText(M)-ExtraBold">
           Let's talk for Something special
         </p>
@@ -16,9 +17,12 @@ const ContactMeView = () => {
           I seek to push the limits of creativity to create high-engaging,
           user-friendly, and memorable interactive experiences.
         </p>
-        <p className="text-Heading-H5-Bold text-Primary-Black">
+        <Link
+          href="mailto:ghkhanimatin@gmail.com"
+          className="text-Heading-H5-Bold text-Primary-Black underline"
+        >
           ghkhanimatin@gmail.com
-        </p>
+        </Link>
       </div>
     </div>
   );
