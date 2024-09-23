862aa81914cd3b337ddcaa94d98a5014830c67bc
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-23 11:01:34 +0330

    edit textsrea height

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[60px] w-full resize-none rounded-md border-2 border-Primary-Black bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground hover:bg-Zinc-100 focus:shadow-md disabled:cursor-not-allowed disabled:border-Zinc-200 disabled:bg-Zinc-100 disabled:text-Zinc-300",
+          "flex min-h-36 w-full resize-none rounded-md border-2 border-Primary-Black bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground hover:bg-Zinc-100 focus:shadow-md disabled:cursor-not-allowed disabled:border-Zinc-200 disabled:bg-Zinc-100 disabled:text-Zinc-300",
           className,
         )}
         ref={ref}
