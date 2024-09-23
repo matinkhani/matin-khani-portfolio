eff94631f690ec6e7029ccd785e91ceddb85b345
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-23 11:01:19 +0330

    edit input height

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-sm border-2 border-Primary-Black bg-transparent px-3 py-1 text-sm text-Primary-Black shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-Zinc-500 hover:bg-Zinc-100 focus:shadow-md disabled:cursor-not-allowed disabled:border-Zinc-200 disabled:bg-Zinc-100 disabled:text-Zinc-300",
+          "flex h-14 w-full rounded-sm border-2 border-Primary-Black bg-transparent px-3 py-1 text-sm text-Primary-Black shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-Zinc-500 hover:bg-Zinc-100 focus:shadow-md disabled:cursor-not-allowed disabled:border-Zinc-200 disabled:bg-Zinc-100 disabled:text-Zinc-300",
           className,
         )}
         ref={ref}
