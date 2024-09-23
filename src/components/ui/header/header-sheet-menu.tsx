5a00ae7eb4ab506302d3a03315b8e12ff8dade8f
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-23 15:15:55 +0330

    modify header shhe menu

--- src/components/ui/header/header-sheet-menu.tsx
+++ src/components/ui/header/header-sheet-menu.tsx
@@ -1,28 +1,47 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import IconBurgurMenu from "@/components/icons/icon-burgur-menu";
+import { HeaderItems } from "./header-items";
+import HeaderDownloadBtn from "./header-download-btn";
 
 const HeaderSheetMenu = () => {
+  const sheetRef = useRef<HTMLDivElement | null>(null);
+
+  const handleLinkClick = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+    if (sheetRef.current) {
+      sheetRef.current.click(); // This will trigger the close button of the Sheet
+    }
+  };
+
   return (
     <Sheet>
       <SheetTrigger className="block md:hidden">
         <IconBurgurMenu />
       </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
+      <SheetContent ref={sheetRef}>
+        <SheetDescription className="mt-11 flex flex-col items-center gap-6">
+          <HeaderDownloadBtn />
+          {HeaderItems.map((item) => (
+            <button
+              key={item.title}
+              onClick={() => handleLinkClick(item.link)}
+              className="text-Heading-H5-Bold"
+            >
+              {item.title}
+            </button>
+          ))}
+        </SheetDescription>
       </SheetContent>
     </Sheet>
   );
