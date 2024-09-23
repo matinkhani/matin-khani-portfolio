44a40c95bfb58febaeb389989fb241f2f5dc09b6
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-09-23 14:42:35 +0330

    modify skill card

--- src/components/ui/card-skills.tsx
+++ src/components/ui/card-skills.tsx
@@ -3,15 +3,16 @@ import React, { FC, ReactNode } from "react";
 type Props = {
   title: string;
   icon?: ReactNode;
+  className?: string;
 };
 
-const CardSkills: FC<Props> = ({ title, icon }) => {
+const CardSkills: FC<Props> = ({ title, icon, className }) => {
   return (
-    <div className="cursor-custom group flex h-28 flex-grow flex-col items-center justify-center rounded border-2 border-Primary-Black bg-Primary-White p-6 hover:bg-Primary-Black md:h-36">
+    <div
+      className={`${className} cursor-custom group z-10 flex h-28 flex-grow flex-col items-center justify-center rounded border-2 border-Primary-Black bg-Primary-White p-6 md:h-36`}
+    >
       {icon}
-      <p className="text-Primary-Black group-hover:text-Primary-White">
-        {title}
-      </p>
+      <p className="text-Primary-Black">{title}</p>
     </div>
   );
 };
